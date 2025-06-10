d27f2d697763cabdcdd35ccb77e4b09adaffa60d
Bhargav Annem <bannem@caltech.edu>
2025-06-10 02:00:16 -0700

    feat: polynomial lib

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -1,10 +1,9 @@
-use std::ops::{Add, Index, Mul};
 use plonky2::field::types::Field;
 use plonky2::field::extension::Extendable;
 use plonky2::hash::hash_types::RichField;
 use plonky2::iop::target::Target;
-use plonky2::iop::witness::{PartialWitness, WitnessWrite};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
+use std::ops::{Add, Index, Mul, Neg, Sub};
 
 use crate::{CURRENT_BUILDER, constants::{F, D}};
 
@@ -149,6 +148,22 @@ impl Add for Polynomial {
             coeffs: result_coeffs,
             targets: result_targets,
         }
+    }
+}
+
+impl Neg for Polynomial {
+    type Output = Self;
+    
+    fn neg(self) -> Self::Output {
+        Self::new(self.coeffs.iter().map(|&c| -c).collect())
+    }
+}
+
+impl Sub for Polynomial {
+    type Output = Self;
+    
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + (-rhs)
     }
 }
 
